b67fee1a1886e749e6d8bc123cfa2e8a3fe5b7d1
marc9622 <maaw@itu.dk>
2023-02-28 01:16:20 +0100

    Update soft_theme.lua

--- colors/soft_theme.lua
+++ colors/soft_theme.lua
@@ -1,7 +1,3 @@
--- You probably always want to set this in your vim file
-vim.opt.background = 'dark'
-vim.g.colors_name = 'soft_theme'
-
 -- By setting our module to nil, we clear lua's cache,
 -- which means the require ahead will *always* occur.
 --
@@ -14,8 +10,8 @@ vim.g.colors_name = 'soft_theme'
 --
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
-package.loaded['lush_theme.soft_theme'] = nil
+package.loaded['lush_theme/soft_theme'] = nil
 
 -- include our theme file and pass it to lush to apply
-require('lush')(require('soft_theme/lua/lush_theme/soft_theme.lua'))
+require('lush')(require('lush_theme/soft_theme'))
 
